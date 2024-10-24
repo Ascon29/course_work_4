7cd42f5688f483f6758175de4ea525b82a00164c
Виктор Бугай <ascon2913@gmail.com>
2024-10-24 20:55:19 +0500

    models, views, admin created

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from newsletter.models import Recipient, Message, NewsLetter, Attempt
+
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'comment',)
+    search_fields = ('name',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('head', 'body',)
+
+
+@admin.register(NewsLetter)
+class NewsLetterAdmin(admin.ModelAdmin):
+    list_display = ('message',)
